dd0f70da0ba02696dc9c529cb9e209293eab4731
Can Babaoglu <babaoglu.can@gmail.com>
2015-01-10 14:51:09 +0000

    First commit

---
+++ app/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en-us" ng-app="sprongoPayouts"> 
+<head>
+    <title>Sprongo payouts panel </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta charset="UTF-8">
+    <base href="/" />
+
+    <!-- CSS FILES -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">  
+
+</head>
+
+<body>
+	
+    <h1>Hello</h1>
+	<div ng-view></div>
+
+    <!-- JS LIBRARIES -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="//code.angularjs.org/1.3.0-rc.2/angular.min.js"></script>
+    <script src="//code.angularjs.org/1.3.0-rc.2/angular-route.min.js"></script>
+
+
+</body>
